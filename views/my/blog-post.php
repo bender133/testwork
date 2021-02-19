1c8ddd6196a09920a3b2c231db982dbb30b5e5c4
bender133 <bender133@bk.ru>
2021-02-19 22:18:21 +0300

    прописаны пути к ядру

--- views/my/blog-post.php
+++ views/my/blog-post.php
@@ -2,9 +2,9 @@
 <h1>Blog Post</h1>
 <h1>Blog Post</h1>
 <h1>Blog Post</h1>
-<h1>Blog Post</h1>
-<h1>Blog Post</h1>
-<h1>Blog Post</h1>
+    <h1>Blog Post</h1>
+        <h1>Blog Post</h1>
+    <h1>Blog Post</h1>
 <h1>Blog Post</h1>
 <h1>Blog Post
     <p></p></h1>
